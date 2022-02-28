ba4741338ec1fb4707b5d35a546ccb546aa10ec0
jlloaiza <jlloaiza04@gmail.com>
2022-02-28 18:39:50 -0500

    avatar img pedal

--- src/app/client-view/landing/landing.component.ts
+++ src/app/client-view/landing/landing.component.ts
@@ -20,8 +20,9 @@ export class LandingComponent implements OnInit {
 
   getParameters(){
     this.cards = [{img:"./assets/img/favicon-512.png",          name:"McDonalds",url:"#restaurant/0",backg:"./assets/img/mcdonaldsfotoprincipal.jpg"}, 
-                  {img:"./assets/img/logo-frisby-1024x1024.jpg",name:"Frisby",   url:"#restaurant/1",backg:"./assets/img/pollo_frisby.jpg"},
+                  {img:"https://firebasestorage.googleapis.com/v0/b/bonapettit-app.appspot.com/o/6ClOZsipScOpDMG8GzdW%2Flogo-frisby-1024x1024.jpg?alt=media&token=2a840cae-5844-4056-8b99-7fb675cbb435",name:"Frisby",   url:"#restaurant/1",backg:"https://firebasestorage.googleapis.com/v0/b/bonapettit-app.appspot.com/o/6ClOZsipScOpDMG8GzdW%2Fpollo_frisby.jpg?alt=media&token=1d2c6aa8-99ba-481e-84cd-838ff5825d66"},
                   {img:"./assets/img/torigallo.jpeg",           name:"Torigallo",url:"#restaurant/2",backg:"./assets/img/chorizo.jpeg"}];
     this.part_num = 15;
   }
+  
 }
