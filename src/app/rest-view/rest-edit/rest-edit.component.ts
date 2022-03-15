3eff08a3bc6476857be3759a5c37f03f5e07c9c5
Jorge Loaiza <jlloaiza04@gmail.com>
2022-03-14 22:37:51 -0500

    restaurant-edit v0.1

--- src/app/rest-view/rest-edit/rest-edit.component.ts
+++ src/app/rest-view/rest-edit/rest-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PageApiService } from 'app/client-view/services/get/page-api.service';
@@ -10,9 +10,35 @@ import { PageApiService } from 'app/client-view/services/get/page-api.service';
   styleUrls: ['./rest-edit.component.css']
 })
 export class NgbdModalContent {
-  @Input() dish;
+  @Input() dish: any;
+
+  @Output() updatedDishEvent = new EventEmitter();
+
+  @ViewChild("dishDescription") dishDescription: ElementRef;
+  @ViewChild("dishName") dishName: ElementRef;
+  @ViewChild("dishPrice") dishPrice: ElementRef;
 
   constructor(public activeModal: NgbActiveModal) { }
+
+  changeTxt() {
+    console.log(this.dishName.nativeElement.innerHTML);
+  }
+
+  sendChanges() {
+    let updatedDish = {
+      id: "",
+      name: "",
+      descripcion: "",
+      precio: Number,
+    };
+    updatedDish.id = this.dish.id;
+    updatedDish.name = this.dishName.nativeElement.innerHTML;
+    updatedDish.descripcion = this.dishDescription.nativeElement.innerHTML;
+    updatedDish.precio = this.dishPrice.nativeElement.innerHTML;
+    //console.log(updatedDish)
+    this.activeModal.close(updatedDish);
+  }
+
 }
 
 
@@ -37,16 +63,65 @@ export class RestEditComponent implements OnInit {
     this.apiservice.getRestaurantData(path).subscribe((restaurant) => {
       this.page = restaurant;
     });
-    console.log(this.page);
+
+
   }
 
   isPremium() {
     return false;
   }
 
-  openModal(dish) {
+  uploadChanges(dish: any) {
+    console.log(dish);
+  }
+
+  openDetailModal(dish: any) {
     const modalRef = this.modalService.open(NgbdModalContent);
     modalRef.componentInstance.dish = dish;
+    modalRef.result.then(function (result) {
+      console.log(result);
+    });
+  }
+
+  openEditModal(dish: any) {
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.dish = dish;
+
+    let newDish:any;
+    modalRef.result.then(function (result) {
+      newDish = result;
+      
+      //getDish(saveDishInDatabase, result);
+    });
+
+    let index = this.page.categories.findIndex( ({ id }) => id === dish.id);
+    this.page.categories[index] = dish;
+
+    console.log(newDish);
+    console.log(this.page.categories[index]);
+    
+    /*
+    var getDish = function (callback, dish) {
+      // get the username somehow
+      var username = dish;
+      callback(username);
+    };
+
+    var saveDishInDatabase = function (dish, categories) {
+      let index = categories.findIndex( ({ id }) => id === dish.id);
+      categories[index] = dish;
+    };
+    */
+
+    
+  }
+
+  openCreateModal(dish: any) {
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.dish = dish;
+    modalRef.result.then(function (result) {
+      console.log(result);
+    })
   }
 
 }
