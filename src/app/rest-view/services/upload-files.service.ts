0e79c66099e463c279c4cff0db586e7cec027b96
Jorge Loaiza <jlloaiza04@gmail.com>
2022-03-20 14:54:16 -0500

    d

--- src/app/rest-view/services/upload-files.service.ts
+++ src/app/rest-view/services/upload-files.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from "environments/environment";
@@ -7,16 +7,24 @@ import { environment } from "environments/environment";
   providedIn: 'root'
 })
 export class UploadFilesService {
-  private baseUrl = "http://".concat(environment.back_url, ":", environment.back_port);
+  private baseUrl = "".concat(environment.back_url, ":", environment.back_port);
 
   constructor(private http: HttpClient) { }
 
-  upload(file: File): Observable<HttpEvent<any>> {
+  upload(file: File, pageID:string): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
     formData.append('file', file);
-    const req = new HttpRequest('POST', `${this.baseUrl}/upload-img`, formData, {
+    
+    const req = new HttpRequest('POST', this.baseUrl.concat('/upload-img'), formData, {
+      headers: new HttpHeaders({userid: pageID}),
       reportProgress: true,
-      responseType: 'json'
+      responseType: 'json',
+    });
+    console.log(this.baseUrl.concat('/upload-img'));
+    this.http.post(this.baseUrl.concat('/upload-img'), formData, {
+      headers: new HttpHeaders({userid: pageID}),
+      reportProgress: true,
+      responseType: 'json',
     });
     return this.http.request(req);
   }
